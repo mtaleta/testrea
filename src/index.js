3ae673b8c3b3b626a5a2901db2175824eda18f66
Elon Chiu <elon.chiu@tpinformation.com.tw>
2017-11-15 18:28:56 +0800

    1.2.3 1115 add Redux DevTools Extension

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import reducer from './reducers'
 
 // import 'bootstrap/dist/css/bootstrap.css'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 render(
   <Provider store={store}>
