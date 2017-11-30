fdbed97acf7e5004f0a91569ee27ef3c49bf2f20
mtal <ex051240@gmail.com>
2017-12-01 01:22:23 +0800

    part2

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure, addDecorator } from '@storybook/react';
-import React from 'react';
-
-const req = require.context('../src/stories', true, /.js$/);
-const loadStories = () => {
-  req.keys().forEach(filename => req(filename))
-}
-configure(loadStories, module);
